66f338a69ea108b1a5df88a977d8fd122335bfbf
jxgu1016 <jxgu1016@gmail.com>
2018-03-20 21:33:06 +0800

    Specify the backward process

--- CenterLoss.py
+++ CenterLoss.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+from torch.autograd.function import Function
+
 
 class CenterLoss(nn.Module):
     def __init__(self, num_classes, feat_dim, loss_weight=1.0):
@@ -10,41 +12,46 @@ class CenterLoss(nn.Module):
         self.loss_weight = loss_weight
         self.centers = nn.Parameter(torch.randn(num_classes, feat_dim))
         # self.register_parameter('centers', self.centers) # no need to register manually. See nn.Module.__setattr__(...)
-        self.use_cuda = False
+        self.centerlossfunction = CenterlossFunction.apply
 
     def forward(self, y, feat):
-        # torch.histc can only be implemented on CPU
-    	# To calculate the total number of every class in one mini-batch. See Equation 4 in the paper
-        if self.use_cuda:
-            hist = Variable(torch.histc(y.cpu().data.float(),bins=self.num_classes,min=0,max=self.num_classes) + 1).cuda()
-        else:
-            hist = Variable(torch.histc(y.data.float(),bins=self.num_classes,min=0,max=self.num_classes) + 1)
-
-        centers_count = hist.index_select(0,y.long())
-
-
         # To squeeze the Tenosr
-        batch_size = feat.size()[0]
+        batch_size = feat.size(0)
         feat = feat.view(batch_size, 1, 1, -1).squeeze()
         # To check the dim of centers and features
-        if feat.size()[1] != self.feat_dim:
+        if feat.size(1) != self.feat_dim:
             raise ValueError("Center's dim: {0} should be equal to input feature's dim: {1}".format(self.feat_dim,feat.size()[1]))
+        return self.loss_weight * self.centerlossfunction(feat, y, self.centers)
 
-        centers_pred = self.centers.index_select(0, y.long())
-        diff = feat - centers_pred
-        loss = self.loss_weight * 1 / 2.0 * (diff.pow(2).sum(1) / centers_count).sum()
-        return loss
 
-    def cuda(self, device_id=None):
-        """Moves all model parameters and buffers to the GPU.
+class CenterlossFunction(Function):
+    @staticmethod
+    def forward(ctx, feature, label, centers,):
+        ctx.save_for_backward(feature, label, centers)
+        centers_pred = centers.index_select(0, label.long())
+        # print centers_pred
+        return(feature - centers_pred).pow(2).sum(1).sum(0) 
+        # a bug that torch.sum() not return a Tensor. 
+        # See "https://discuss.pytorch.org/t/torch-sum-not-return-a-tensor/15200/5"
 
-        Arguments:
-            device_id (int, optional): if specified, all parameters will be
-                copied to that device
-        """
-        self.use_cuda = True
-        return self._apply(lambda t: t.cuda(device_id))
-
+    @staticmethod
+    def backward(ctx, grad_output):
+        feature, label, centers = ctx.saved_variables
+        grad_feature = feature - centers.index_select(0, label.long())
+        # print 'grad_feature:', grad_feature
+        grad_centers = torch.zeros(centers.size())
+        counts = torch.ones(centers.size(0))
+        if feature.data.type() != 'torch.FloatTensor':
+            grad_centers = grad_centers.cuda()
+            counts = counts.cuda()
+        for i in range(feature.size(0)):
+            j = int(label[i].data[0])
+            # print j
+            counts[j] += 1
+            grad_centers[j] += (centers.data[j] - feature.data[i])
+        # print counts
+        grad_centers = Variable(grad_centers/counts.view(-1, 1))
+        return grad_feature, None, grad_centers
 
 
 def main():
@@ -54,16 +61,30 @@ def main():
 
     print ct.centers.grad
 
-    y = Variable(torch.Tensor([0,0,0,1]))#.cuda()
+    y = Variable(torch.Tensor([0,0,2,1]))#.cuda())
     # print y
-    feat = Variable(torch.zeros(4,2),requires_grad=True)#.cuda()
+    feat = Variable(torch.zeros(4,2),requires_grad=True)
     # print feat
 
     out = ct(y,feat)
     out.backward()
     print ct.centers.grad
+    print feat.grad
+
+def test_function():
+    centers = Variable(torch.randn(10,2).cuda(),requires_grad=True)
+    print centers
+    feat = Variable(torch.zeros(4,2).cuda(),requires_grad=True)
+    y = Variable(torch.Tensor([0,0,2,1]).cuda())
+    centerlossfunction = CenterlossFunction.apply
+    loss = centerlossfunction(feat, y, centers)
+    print loss
+    loss.backward()
+    print feat.grad
+    print centers.grad
 
 
 
 if __name__ == '__main__':
     main()
+    # test_function()
