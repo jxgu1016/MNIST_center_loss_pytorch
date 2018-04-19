ac029f345a7402117224052d729e7ce4197a4e9f
Bran Stewart <wenfahu@outlook.com>
2018-04-19 10:23:28 +0800

    Backprop optimization (#3)
    * opt for loo
    
    * typo fix
    
    * add cuda
    
    * div by counts
    
    * fix error: center.size -> label.size

--- CenterLoss.py
+++ CenterLoss.py
@@ -5,55 +5,47 @@ from torch.autograd.function import Function
 
 
 class CenterLoss(nn.Module):
-    def __init__(self, num_classes, feat_dim):
+    def __init__(self, num_classes, feat_dim ):
         super(CenterLoss, self).__init__()
-        self.num_classes = num_classes
-        self.feat_dim = feat_dim
         self.centers = nn.Parameter(torch.randn(num_classes, feat_dim))
-        # self.register_parameter('centers', self.centers) # no need to register manually. See nn.Module.__setattr__(...)
-        self.centerlossfunction = CenterlossFunction.apply
+        self.centerlossfunc = CenterlossFunc.apply
+        self.feat_dim = feat_dim
 
-    def forward(self, y, feat):
-        # To squeeze the Tenosr
+    def forward(self, label, feat):
         batch_size = feat.size(0)
         feat = feat.view(batch_size, 1, 1, -1).squeeze()
         # To check the dim of centers and features
         if feat.size(1) != self.feat_dim:
             raise ValueError("Center's dim: {0} should be equal to input feature's dim: {1}".format(self.feat_dim,feat.size(1)))
-        return self.centerlossfunction(feat, y, self.centers)
+        return self.centerlossfunc(feat, label, self.centers)
 
 
-class CenterlossFunction(Function):
+class CenterlossFunc(Function):
 
     @staticmethod
     def forward(ctx, feature, label, centers):
         ctx.save_for_backward(feature, label, centers)
-        centers_pred = centers.index_select(0, label.long())
-        return (feature - centers_pred).pow(2).sum(1).sum(0) / 2.0
-        # a bug that torch.sum() not return a Tensor and will be fixed in PyTorch 0.4+
-        # See "https://discuss.pytorch.org/t/torch-sum-not-return-a-tensor/15200/5"
+        centers_batch = centers.index_select(0, label.long())
+        return (feature - centers_batch).pow(2).sum(1).sum(0) / 2.0
+
 
     @staticmethod
     def backward(ctx, grad_output):
-        # feature, label, centers = ctx.saved_variables
         feature, label, centers = ctx.saved_tensors
-        grad_feature = feature - centers.index_select(0, label.long()) # Eq. 3
-
+        centers_batch = centers.index_select(0, label.long())
+        diff = centers_batch - feature
         # init every iteration
         counts = centers.new(centers.size(0)).fill_(1)
+        ones = centers.new(label.size(0)).fill_(1)
         grad_centers = centers.new(centers.size()).fill_(0)
+        counts = counts.scatter_add_(0, label.long(), ones)
         # print counts, grad_centers
+        grad_centers.scatter_add_(0, label.unsqueeze(1).expand(feature.size()).long(), diff)
 
-        # Eq. 4 || need optimization !! To be vectorized, but how?
-        for i in range(feature.size(0)):
-            j = int(label[i])
-            counts[j] += 1
-            grad_centers[j] += (centers[j] - feature[i])
-        # print counts
+
         grad_centers = grad_centers/counts.view(-1, 1)
 
-        return Variable(grad_feature * grad_output.data), None, Variable(grad_centers) # grad_centers need to mul grad_output(loss_weight)??? I think not.
-
+        return Variable(-grad_output.data*diff), None, Variable(grad_centers)
 
 def main(test_cuda=False):
     print('-'*80)
@@ -64,17 +56,18 @@ def main(test_cuda=False):
         ct = ct.cuda()
         y = Variable(torch.Tensor([0,0,2,1]).cuda())
         feat = Variable(torch.zeros(4,2).cuda(),requires_grad=True)
-    print list(ct.parameters())
-    print ct.centers.grad  
+    print (list(ct.parameters()))
+    print (ct.centers.grad)
     # print y
     # print feat
     out = ct(y,feat)
     out.backward()
-    print ct.centers.grad
-    print feat.grad
+    print(ct.centers.grad)
+    print (feat.grad)
 
 
 if __name__ == '__main__':
+    torch.manual_seed(999)
     main(test_cuda=False)
     if torch.cuda.is_available():
         main(test_cuda=True)
