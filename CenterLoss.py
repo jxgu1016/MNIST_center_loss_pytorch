851a3b800b9f29feb23153e37ea5abf41e0237de
Jiaxin Gu <jxgu1016@gmail.com>
2018-10-05 14:33:51 +0800

    Average grad for centers (#11)
    * ready
    
    * done
    
    * new -> new_*

--- CenterLoss.py
+++ CenterLoss.py
@@ -15,39 +15,40 @@ class CenterLoss(nn.Module):
         feat = feat.view(batch_size, -1)
         # To check the dim of centers and features
         if feat.size(1) != self.feat_dim:
-            raise ValueError("Center's dim: {0} should be equal to input feature's dim: {1}".format(self.feat_dim,feat.size(1)))
-        loss = self.centerlossfunc(feat, label, self.centers)
-        loss /= (batch_size if self.size_average else 1)
+            raise ValueError("Center's dim: {0} should be equal to input feature's \
+                            dim: {1}".format(self.feat_dim,feat.size(1)))
+        batch_size_tensor = feat.new_empty(1).fill_(batch_size if self.size_average else 1)
+        loss = self.centerlossfunc(feat, label, self.centers, batch_size_tensor)
         return loss
 
 
 class CenterlossFunc(Function):
     @staticmethod
-    def forward(ctx, feature, label, centers):
-        ctx.save_for_backward(feature, label, centers)
+    def forward(ctx, feature, label, centers, batch_size):
+        ctx.save_for_backward(feature, label, centers, batch_size)
         centers_batch = centers.index_select(0, label.long())
-        return (feature - centers_batch).pow(2).sum() / 2.0
+        return (feature - centers_batch).pow(2).sum() / 2.0 / batch_size
 
     @staticmethod
     def backward(ctx, grad_output):
-        feature, label, centers = ctx.saved_tensors
+        feature, label, centers, batch_size = ctx.saved_tensors
         centers_batch = centers.index_select(0, label.long())
         diff = centers_batch - feature
         # init every iteration
-        counts = centers.new(centers.size(0)).fill_(1)
-        ones = centers.new(label.size(0)).fill_(1)
-        grad_centers = centers.new(centers.size()).fill_(0)
+        counts = centers.new_ones(centers.size(0))
+        ones = centers.new_ones(label.size(0))
+        grad_centers = centers.new_zeros(centers.size())
 
         counts = counts.scatter_add_(0, label.long(), ones)
         grad_centers.scatter_add_(0, label.unsqueeze(1).expand(feature.size()).long(), diff)
         grad_centers = grad_centers/counts.view(-1, 1)
-        return - grad_output * diff, None, grad_centers
+        return - grad_output * diff / batch_size, None, grad_centers / batch_size, None
 
 
 def main(test_cuda=False):
     print('-'*80)
     device = torch.device("cuda" if test_cuda else "cpu")
-    ct = CenterLoss(10,2).to(device)
+    ct = CenterLoss(10,2,size_average=True).to(device)
     y = torch.Tensor([0,0,2,1]).to(device)
     feat = torch.zeros(4,2).to(device).requires_grad_()
     print (list(ct.parameters()))
